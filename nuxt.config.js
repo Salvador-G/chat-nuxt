460151d7e7ffbda451b267fcff8eae386c613cb7
Salvador-G <sal.guerrero.h26@gmail.com>
2025-01-04 15:29:20 -0500

    nuevo dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,9 @@
 export default {
+  // Configuración del router para especificar el subdirectorio (si es necesario)
+  router: {
+    base: '/chat-nuxt/' // Asegúrate de poner el nombre de tu repositorio en GitHub aquí.
+  },
+
   buildModules: [
     '@nuxtjs/vuetify'
   ],
