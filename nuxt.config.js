3d7af3ecf60f57a34a433938a0904127d4ec0ef8
Salva-GH <84592633+Salvador-G@users.noreply.github.com>
2025-09-28 12:53:41 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
 export default {
   // Configuración del router para especificar el subdirectorio (si es necesario)
-  router: {
-    base: '/chat-nuxt/' // Asegúrate de poner el nombre de tu repositorio en GitHub aquí.
-  },
-
+  target: 'static',
   buildModules: [
     '@nuxtjs/vuetify'
   ],
